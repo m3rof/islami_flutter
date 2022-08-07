844fbd1060838f2ebdda5d5465818f34effd015b
m3roof <maroufa664@gmail.com>
2022-08-07 16:50:41 +0200

    add button to make counter=0

--- lib/home/sebha/Sebha.dart
+++ lib/home/sebha/Sebha.dart
@@ -1,3 +1,4 @@
+import 'dart:ffi';
 
 import 'package:flutter/material.dart';
 
@@ -7,9 +8,9 @@ class Sebha extends StatefulWidget {
 }
 
 class _SebhaState extends State<Sebha> {
-String counter='0';
-String tasbih='سبحان الله';
-double angle=20;
+  String counter = '0';
+  String tasbih = 'سبحان الله';
+  double angle = 20;
 
   @override
   Widget build(BuildContext context) {
@@ -18,70 +19,102 @@ double angle=20;
         child: Column(
           mainAxisAlignment: MainAxisAlignment.start,
           children: [
-            SizedBox(height: 50,),
-
+            SizedBox(
+              height: 50,
+            ),
             InkWell(
-              onTap:IncreaseCounter,
+              onTap: IncreaseCounter,
               child: Transform.rotate(
                   angle: angle,
-                  child: Image.asset('assets/images/body_sebha_logo.png',width: 150,height: 150,)),
+                  child: Image.asset(
+                    'assets/images/body_sebha_logo.png',
+                    width: 150,
+                    height: 150,
+                  )),
             ),
-            SizedBox(height: 30,),
-            Text('عدد التسبيحات',style: TextStyle(fontSize: 25,fontWeight: FontWeight.w500),),
-            SizedBox(height: 15,),
+            SizedBox(
+              height: 30,
+            ),
+            Text(
+              'عدد التسبيحات',
+              style: TextStyle(fontSize: 25, fontWeight: FontWeight.w500),
+            ),
+            SizedBox(
+              height: 15,
+            ),
             Container(
               padding: EdgeInsets.all(5),
               width: 60,
               height: 40,
               decoration: BoxDecoration(
-                color: Color(0xffB7935F),
-                borderRadius: BorderRadius.all(Radius.circular(15)),
-                border: Border.all(
-                  color: Color(0xffB7935F)
-                )
+                  color: Color(0xffB7935F),
+                  borderRadius: BorderRadius.all(Radius.circular(15)),
+                  border: Border.all(color: Color(0xffB7935F))),
+              child: Text(
+                counter,
+                style: TextStyle(fontWeight: FontWeight.w500, fontSize: 25),
+                textAlign: TextAlign.center,
               ),
-              child: Text(counter,style: TextStyle(fontWeight: FontWeight.w500,fontSize: 25),textAlign: TextAlign.center,),
             ),
-            SizedBox(height:20),
+            SizedBox(height: 20),
             Container(
               margin: EdgeInsets.all(2),
               width: 140,
               decoration: BoxDecoration(
-                color: Color(0xffB7935F),
-                borderRadius: BorderRadius.all(Radius.circular(25)),
-                border: Border.all(
-                  color: Color(0xffB7935F)
-                )
-              ),
-              child: Text(tasbih,style: TextStyle(color:Colors.white ,fontWeight: FontWeight.w500,fontSize: 25),
+                  color: Color(0xffB7935F),
+                  borderRadius: BorderRadius.all(Radius.circular(25)),
+                  border: Border.all(color: Color(0xffB7935F))),
+              child: Text(tasbih,
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.w500,
+                      fontSize: 25),
                   textAlign: TextAlign.center),
-            )
-
-
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            ElevatedButton(
+                style: ButtonStyle(
+                    backgroundColor:
+                        MaterialStateProperty.all(Color(0xffB7935F))),
+                onPressed: ClickedButtonTasbih,
+                child: Text(
+                  'تصفير',
+                  style: TextStyle(fontSize: 25, fontWeight: FontWeight.w500,),
+                  textAlign: TextAlign.center,
+                ))
           ],
         ),
       ),
     );
   }
 
-  void IncreaseCounter(){
-    int counterint=int.parse(counter);
-   counterint++;
-   counter=counterint.toString();
-   TasbihString();
-   angle=angle*20;
-   setState(() {});
+  void IncreaseCounter() {
+    int counterint = int.parse(counter);
+    counterint++;
+    counter = counterint.toString();
+    TasbihString();
+    angle = angle * 20;
+    setState(() {});
   }
-  void TasbihString(){
-    int counterint=int.parse(counter);
-    if(counterint<=33){
-      tasbih='سبحان الله';
-    }else if(counterint>33&&counterint<=66){
-      tasbih='الحمد لله';
-    }else if(counterint>66&&counterint<=99){
-      tasbih='الله اكبر';
-    }else if(counterint>99){
-      tasbih='لا إله إلا الله';
+
+  void TasbihString() {
+    int counterint = int.parse(counter);
+    if (counterint <= 33) {
+      tasbih = 'سبحان الله';
+    } else if (counterint > 33 && counterint <= 66) {
+      tasbih = 'الحمد لله';
+    } else if (counterint > 66 && counterint <= 99) {
+      tasbih = 'الله اكبر';
+    } else if (counterint > 99) {
+      tasbih = 'لا إله إلا الله';
     }
+  }
+
+  void ClickedButtonTasbih() {
+    counter = '0';
+    tasbih = 'سبحان الله';
+    setState(() {});
   }
 }
